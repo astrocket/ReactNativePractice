eae0c3b9eb82229145269a080525f05c15b81afd
Mac <mac@ASTROCKET-MAC15.local>
2017-01-10 09:57:19 +0900

    first commit

---
+++ src/components/header.js
@@ -0,0 +1,37 @@
+// Import libraries for making a component
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// Make a component
+const Header = (props) => {
+    const { textStyle, viewStyle } = styles;
+    // destructing 해주는 과정임. 이거 안해주면 밑에서
+    // <View style={styles.viewStyle}> 이렇게 불러줘야 됨.
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>{props.headerText}</Text>
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#F8F8F8',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 60,
+        paddingTop: 15,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        elevation: 2,
+        position: 'relative'
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
+
+// Make the component available to other parts of the app
+export default Header;
